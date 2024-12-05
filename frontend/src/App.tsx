6dbf498e3f0cd0c16a4f7d29080901dcf1fe548e
mikaellecam <lecam.mikael@gmail.com>
2024-12-05 23:15:20 +0100

    removed double quotes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Home} from "./pages/Home.tsx";
-import Quiz from "./pages/Quiz.tsx";
+import {Home} from './pages/Home.tsx';
+import Quiz from './pages/Quiz.tsx';
 
 
 const App = () => {
