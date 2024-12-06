a535e918d4b76504f5c78c674de6442975fed493
mikaellecam <lecam.mikael@gmail.com>
2024-12-06 02:22:54 +0100

    fixed quiz link

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <Link to="/about">Go to Quiz</Link>
+            <Link to="/quiz">Go to Quiz</Link>
         </div>
     );
 };
