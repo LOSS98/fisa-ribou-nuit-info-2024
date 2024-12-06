1bbaef5a7a319fd23901d4dc98014dfb10f871d8
mikaellecam <lecam.mikael@gmail.com>
2024-12-06 02:22:22 +0100

    new layout with navbar and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Home} from './pages/Home.tsx';
 import Quiz from './pages/Quiz.tsx';
+import {Layout} from './components/Layout.tsx';
 
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/quiz" element={<Quiz />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/quiz" element={<Quiz />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 };
