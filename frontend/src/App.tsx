bce6d733dad232c17b79af8ad001123e4a8d4e3d
mikaellecam <lecam.mikael@gmail.com>
2024-12-06 04:27:57 +0100

    added admin routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Home} from './pages/Home.tsx';
 import Quiz from './pages/Quiz.tsx';
 import {Layout} from './components/Layout.tsx';
+import {AdminLogin} from "@/pages/AdminLogin.tsx";
+import {ProtectedRoute} from "@/components/ProtectedRoute.tsx";
+import AdminDashboard from "@/pages/AdminDashboard.tsx";
 
 
 const App = () => {
@@ -11,6 +14,17 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/quiz" element={<Quiz />} />
+
+                    {/* Admin routes */}
+                    <Route path="/admin/login" element={<AdminLogin />} />
+                    <Route
+                        path="/admin/dashboard"
+                        element={
+                            <ProtectedRoute>
+                                <AdminDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
                 </Routes>
             </Layout>
         </BrowserRouter>
